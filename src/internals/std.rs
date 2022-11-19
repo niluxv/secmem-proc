8edcbc73d13e8b3a2fcd243d11fb1679c494716c
niluxv <niluxv.opensource.C-h2ty6xl@yandex.com>
2022-11-19 10:30:31 +0100

    Update rustix, use it to open /proc/self/status

--- src/internals/std.rs
+++ src/internals/std.rs
@@ -7,13 +7,8 @@ pub struct TracerPidNotFound;
 /// `/proc/self/status`. Returns the tracer pid or `0` if not traced.
 #[cfg(target_os = "linux")]
 fn get_tracer_pid() -> anyhow::Result<usize> {
-    // TODO: check that `/proc` is indeed procfs, e.g. use rustix's
-    // `proc_self_status` currently, however, this seems to error when
-    // `/proc/self` is root owned
-
-    // let fd = rustix::io::proc_self_status()?;
-    // let file = std::fs::File::from(fd);
-    let file = std::fs::File::open("/proc/self/status")?;
+    let fd = rustix::io::proc_self_status()?;
+    let file = std::fs::File::from(fd);
     let status = std::io::read_to_string(file)?;
     for line in status.lines() {
         if let Some(tracer_pid_str) = line.strip_prefix("TracerPid:") {
@@ -30,4 +25,18 @@ pub fn is_tracer_present() -> anyhow::Result<Option<rustix::process::RawNonZeroP
     let tracer_pid = get_tracer_pid()?;
     let pid = rustix::process::RawPid::try_from(tracer_pid)?;
     Ok(rustix::process::RawNonZeroPid::new(pid))
+}
+
+#[cfg(test)]
+mod tests {
+    #[cfg(target_os = "linux")]
+    use super::is_tracer_present;
+
+    #[cfg(target_os = "linux")]
+    #[test]
+    fn test_tracer_nonpresent() {
+        assert!(is_tracer_present()
+            .expect("error checking for tracer")
+            .is_none());
+    }
 }
